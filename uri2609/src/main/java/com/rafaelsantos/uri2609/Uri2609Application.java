ff23b234b62a79e3d29f4b5d37aa2b8484141448
Rafael Santos <rafa.05125@gmail.com>
2023-02-03 20:18:16 -0300

    Project created

---
+++ uri2609/src/main/java/com/rafaelsantos/uri2609/Uri2609Application.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.uri2609;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2609Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2609Application.class, args);
+	}
+
+}
