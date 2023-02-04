1512c2048aaa5970853ab8f90dd4a9b67e51eb81
Rafael Santos <rafa.05125@gmail.com>
2023-02-04 16:12:43 -0300

    Category, Product

---
+++ uri2609/src/main/java/com/rafaelsantos/uri2609/entities/Category.java
@@ -0,0 +1,67 @@
+package com.rafaelsantos.uri2609.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+	
+	@Id
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "category")
+	private List<Product> products = new ArrayList<>();
+	
+	public Category() {}
+
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ uri2609/src/main/java/com/rafaelsantos/uri2609/entities/Product.java
@@ -0,0 +1,93 @@
+package com.rafaelsantos.uri2609.entities;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+	
+	@Id
+	private Long id;
+	private String name;
+	private Integer amount;
+	private BigDecimal price;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_categories")
+	private Category category;
+	
+	public Product() {}
+	
+	public Product(Long id, String name, Integer amount, BigDecimal price, Category category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+		this.price = price;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+}
